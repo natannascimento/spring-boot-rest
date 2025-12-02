593bc4f43e4d74f44880aa69b3175bea03f8dee2
Natan Nascimento <natannascimento@outlook.com>
2025-12-02 15:04:04 -0300

    Initial commit

---
+++ src/main/java/com/jobportal/spring_boot_rest/JobRestController.java
@@ -0,0 +1,30 @@
+package com.jobportal.spring_boot_rest;
+
+import com.jobportal.spring_boot_rest.model.JobPost;
+import com.jobportal.spring_boot_rest.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class JobRestController {
+
+    @Autowired
+    private JobService service;
+
+    @GetMapping("jobPosts")
+    //@ResponseBody
+    public List<JobPost> getAllJobs(){
+        return service.getAllJobs();
+    }
+
+    @GetMapping("jobPost/{postId}")
+    public JobPost getJob(@PathVariable("postId") int postId){
+        return service.getJob(postId);
+    }
+}
---
+++ src/main/java/com/jobportal/spring_boot_rest/model/JobPost.java
@@ -0,0 +1,19 @@
+package com.jobportal.spring_boot_rest.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JobPost {
+    private int postId;
+    private String postProfile;
+    private String postDesc;
+    private int reqExperience;
+    private List<String> postTechStack;
+
+}
